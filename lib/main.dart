96e4c207750030ceb58a98903eb7cbfc2c5ee579
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2025-07-27 20:20:55 +0700

    fix: Specify environment file name in dotenv.load for clarity

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
