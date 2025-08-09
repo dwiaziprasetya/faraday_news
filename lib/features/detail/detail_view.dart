6458a0bd49fbdbab627f5e2779381e8900230a55
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2025-08-09 22:31:37 +0700

    update: Update detail_view.dart

--- lib/features/detail/detail_view.dart
+++ lib/features/detail/detail_view.dart
@@ -11,12 +11,38 @@ class DetailView extends StatelessWidget {
       value: SystemUiOverlayStyle.light,
       child: Scaffold(
         backgroundColor: const Color(0xFF09122C),
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.all(16),
+          child: SafeArea(
+            child: SizedBox(
+              width: double.infinity,
+              height: 46,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF6cbd70),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Summarize News',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: 400,
+                height: 450,
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
@@ -58,6 +84,29 @@ class DetailView extends StatelessWidget {
                         ),
                       ),
                     ),
+                    Positioned(
+                      top: MediaQuery.of(context).padding.top + 12,
+                      right: 16,
+                      child: GestureDetector(
+                        onTap: () => Get.back(),
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            // ignore: deprecated_member_use
+                            color: Colors.grey.withOpacity(0.5),
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.share,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -72,19 +121,38 @@ class DetailView extends StatelessWidget {
                         'Tesla will accept Bitcoin as payment, Elon Musk says.',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 24,
+                          fontSize: 26,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       SizedBox(height: 8),
                       Text(
-                        'Trending · 1h ago',
+                        'Yahoo Entertainment · 1 week ago',
                         style: TextStyle(fontSize: 14, color: Colors.white),
                       ),
-                      SizedBox(height: 16),
-                      Text(
-                        'Lets be real: the internet is full of a lot of the same stuff. From derivative content on social media to websites that all look the same, what was once a portal to new worlds and experiences has been feeling a bit dry recently, like Ive seen all this stuff…',
-                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      SizedBox(height: 24),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text:
+                                  'Elon Musk is right to worry about Americas debt problem and bitcoin is no more valuable than a flower, author Nassim Nicholas Taleb said in a wide-ranging interview with Business Insider',
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const TextSpan(text: " "),
+                            TextSpan(
+                              text: "Read more",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Color(0xFF6cbd70),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
